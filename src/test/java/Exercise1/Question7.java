efd7bdad615eb703f987d0df4bc1d211b3023c8f
TharminiNishan <tharmini.it.9220@gmail.com>
2023-01-11 06:06:19 +0530

    Question7
    Question7

--- src/test/java/Exercise1/Question7.java
+++ src/test/java/Exercise1/Question7.java
@@ -3,9 +3,19 @@ package Exercise1;
 public class Question7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Test git");
-System.out.println("git");
-	}
+		String[] arr = {"First", "Second", "Third", "Fourth"};
+        System.out.println("Array before Swap" + "\n");
+        for (String element : arr) {
+            System.out.println(element);
+        }
+        //Simple Swapping logic
+        String temp = arr[1];
+        arr[1] = arr[2];
+        arr[2] = temp;
+        System.out.println("\n" + "Array after Swap" + "\n");
+        for (String element : arr) {
+            System.out.println(element);
+
+        }}
 
 }
