2f8b2c24148ddb8b5993ddf608ecd8229c8a40c1
TharminiNishan <tharmini.it.9220@gmail.com>
2023-01-11 07:27:28 +0530

    E1Q8
    Exercise1 Question8

--- src/test/java/Exercise1/Question8.java
+++ src/test/java/Exercise1/Question8.java
@@ -3,8 +3,32 @@ package Exercise1;
 public class Question8 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        int [] arr = new int [] {1, 2, 3, 4, 5};    
+        int numberOfTimesToBeRotated = 1;  
+           
+        System.out.println("Array before rotation: ");  
+        for (int i = 0; i < arr.length; i++) {   
+            System.out.print(arr[i] + ", ");   
+        }    
+          
+        for(int i = 0; i < numberOfTimesToBeRotated; i++){  
+            int j, storingLast;  
+     
+            storingLast = arr[arr.length-1];  
+          
+            for(j = arr.length-1; j > 0; j--){   
+                arr[j] = arr[j-1];  
+            }  
+            arr[0] = storingLast;  
+        }  
+      
+        System.out.println();  
+          
+        
+        System.out.println("Array after rotation: ");  
+        for(int i = 0; i< arr.length; i++){  
+            System.out.print(arr[i] + " ");  
 
 	}
-
+	}
 }
